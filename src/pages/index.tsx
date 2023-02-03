ec002af2ff2468ab672dbebd1de10a963e7b933a
Vinicius <viniciusflexa@gmail.com>
2023-02-03 17:09:01 -0300

    image implementation + config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,17 +3,18 @@ import File from '../../public/File.svg'
 import Github from '../../public/Github.svg'
 
 import Typewriter from 'typewriter-effect';
+import Image from "next/image";
 
 export default function Home() {
   return (
       <div className="z-0 flex h-screen justify-center text-white">
 
         {/* mobile section */}
-        <div className="flex md:hidden w-full items-center justify-center">
-            <div className="flex w-[80%] flex-col items-center justify-between">
+        <div className="flex md:hidden w-full justify-center">
+            <div className="flex w-[80%] flex-col items-center justify-center gap-3">
 
-                <div>
-                    <p>IMAGEM</p>
+                <div className="border-2 border-[#FF6600] rounded-full p-2">
+                    <Image src="https://avatars.githubusercontent.com/u/71902143?v=4" alt="portrait" width={150} height={150} className="rounded-full" />
                 </div>
 
                 <div className="flex flex-col items-center text-center gap-6">
@@ -40,8 +41,8 @@ export default function Home() {
         </div>
 
         {/* desktop section */}
-        <div className="hidden md:flex w-full items-center justify-center">
-            <div className="flex w-[80%] justify-between">
+        <div className="hidden md:flex w-full justify-center">
+            <div className="flex w-[85%] justify-between items-center">
                 <div className="flex flex-col gap-6">
                     <div>                        
                         <p className="text-[#FF6600] text-6xl">VINICIUS RODRIGUES</p>
@@ -62,8 +63,8 @@ export default function Home() {
                     </div>
                 </div>
 
-                <div>
-                    <p>IMAGEM</p>
+                <div className="border-4 border-[#FF6600] rounded-full p-6 translate-x-1 translate-y-1 hover:-translate-x-1 hover:-translate-y-1 transition-all">
+                    <Image src="https://avatars.githubusercontent.com/u/71902143?v=4" alt="portrait" width={350} height={350} className="rounded-full" />
                 </div>
             </div>
         </div>
