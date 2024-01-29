29103978dc247a079efe3da401812d15e47b63fa
Vinicius <viniciusflexa@gmail.com>
2024-01-29 12:39:01 -0300

    update projects

--- src/components/Projeto.tsx
+++ src/components/Projeto.tsx
@@ -4,17 +4,88 @@ import Link from "next/link"
 interface ProjetoProps{
     link: string
     title: string
+    descBr: string
+    descEn: string
     img: string
+    idioma: string
+    inverted: Boolean
+    tech: any
 }
 
 export function Projeto(props: ProjetoProps){
     return(
-        <Link href={props.link} target="_blank" className="flex w-full justify-center items-center hover:scale-105 transition-all" >                    
-            <div className="flex flex-col gap-2 transition-all bg-[#0F0F17] p-2 rounded-lg">
-                <Image src={props.img} alt="Projeto" width={400} height={400} className="rounded-lg shadow-md" />
-                <p className="text-xl lg:text-2xl">
-                    {props.title}
-                </p>
+        <Link href={props.link} target="_blank" className="flex w-full justify-center">   
+            <div className="flex flex-col lg:flex-row lg:w-full items-center lg:justify-between bg-[#0F0F17] p-2 gap-4 lg:gap-0 rounded-lg shadow-md">
+                {props.inverted ? (
+                    <>                    
+                        <div className="hidden lg:flex flex-col lg:w-1/2 h-full justify-evenly text-center lg:text-start lg:pl-6">
+                            <h1 className="flex text-2xl font-medium">{props.title}</h1>
+
+                            {props.idioma == 'brasil' ? (
+                                <p className="flex pr-6">{props.descBr}</p>
+                            ) : (
+                                <p className="flex pr-6">{props.descEn}</p>
+                            )}
+
+                            <div className="w-full bg-slate-800 text- h-1 rounded-full" />
+
+                            <div className="flex flex-col w-full gap-4 pb-2">
+                                {props.idioma == 'brasil' ? (
+                                        <p className="text-sm">Tecnologias usadas:</p>
+                                    ) : (
+                                        <p className="text-sm">Tech used:</p>
+                                )}
+                                <div className="flex w-full items-center gap-4">
+                                    {props.tech}
+                                </div>
+                            </div>
+                        </div>
+
+                        <Image src={props.img} alt="Projeto" width={600} height={600} className="rounded-lg" quality={100} />
+
+                        <h1 className="flex lg:hidden text-2xl font-medium">{props.title}</h1>
+
+                        <div className="lg:hidden flex w-[80%] bg-slate-800 text- h-1 rounded-full" />
+
+                        <div className="lg:hidden flex flex-col w-full lg:justify-end justify-center items-center gap-4 pb-2">
+                            {props.idioma == 'brasil' ? (
+                                    <p className="text-sm">Tecnologias usadas:</p>
+                                ) : (
+                                    <p className="text-sm">Tech used:</p>
+                            )}
+                            <div className="flex w-full lg:justify-end justify-center items-center gap-4">
+                                {props.tech}
+                            </div>
+                        </div>
+                    </>
+                ) : (
+                    <>                    
+                        <Image src={props.img} alt="Projeto" width={600} height={600} className="rounded-lg" quality={100} />
+
+                        <div className="flex flex-col lg:w-1/2 w-full h-full lg:justify-evenly text-center lg:text-end lg:pr-6 gap-4">
+                            <h1 className="text-2xl font-medium">{props.title}</h1>
+
+                            {props.idioma == 'brasil' ? (
+                                <p className="hidden lg:flex pl-6">{props.descBr}</p>
+                            ) : (
+                                <p className="hidden lg:flex pl-6">{props.descEn}</p>
+                            )}
+
+                            <div className="lg:w-full w-[80%] self-center bg-slate-800 h-1 rounded-full" />
+
+                            <div className="flex flex-col w-full lg:justify-end gap-4 pb-2">
+                                {props.idioma == 'brasil' ? (
+                                        <p className="text-sm">Tecnologias usadas:</p>
+                                    ) : (
+                                        <p className="text-sm">Tech used:</p>
+                                )}
+                                <div className="flex w-full lg:justify-end justify-center items-center gap-4">
+                                    {props.tech}
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                )}
             </div>
         </Link>
     )
--- src/components/Projetos.tsx
+++ src/components/Projetos.tsx
@@ -1,5 +1,20 @@
 import { Projeto } from "./Projeto";
-import { ProjetoPrincipal } from "./ProjetoPrincipal";
+
+import react from '../../public/react 1.svg'
+import next from '../../public/nextdotjs 1.svg'
+import ts from '../../public/typescript 1.svg'
+import twcss from '../../public/tailwindcss 1.svg'
+import firebase from '../../public/firebase.svg'
+import vite from '../../public/vite.svg'
+import html from '../../public/html5 1.svg'
+import css from '../../public/css3 1.svg'
+import js from '../../public/javascript 1.svg'
+import figma from '../../public/figma 1.svg'
+import git from '../../public/git 1.svg'
+
+import Image from "next/image";
+import Link from "next/link";
+import { ArrowRight, GithubLogo } from "phosphor-react";
 
 interface ProjetosProps{
     idioma?: string
@@ -14,8 +29,8 @@ export function Projetos(props: ProjetosProps){
                 <p className="text-4xl lg:text-6xl p-6 bg-white text-[#FF6600] rounded-full shadow-lg">PROJECTS</p>
             )}
 
-            <div className="flex flex-col w-full items-center justify-center gap-5">
-                <ProjetoPrincipal 
+            <div className="flex flex-col w-full items-center justify-center gap-5 my-5">
+                <Projeto 
                     link="https://bliss-bem-estar.vercel.app" 
                     title="Bliss"
                     descBr="Bliss é uma aplicação desenvolvida para ajudar na jornada pessoal de bem-estar do usuário. Através da plataforma, o usuário pode ter acesso a diversas ferramentas e recursos para melhorar sua saúde mental e física, incluindo frases diárias, criação de hábitos, diário e contatos de ajuda psicológica." 
@@ -23,27 +38,80 @@ export function Projetos(props: ProjetosProps){
                     img="https://i.imgur.com/UiYcqkY.png"
                     idioma={props.idioma!}
                     inverted={false}
+                    tech={
+                        <>
+                            <Image src={next} alt='tech' width={30} height={30} />
+                            <Image src={ts} alt='tech' width={30} height={30} />
+                            <Image src={twcss} alt='tech' width={30} height={30} />
+                            <Image src={firebase} alt='tech' width={30} height={30} />
+                        </>
+                    }
                 />
 
-                <ProjetoPrincipal 
+                <Projeto 
+                    link="https://alerta-rosa.vercel.app" 
+                    title="Alerta Rosa"
+                    descBr="Alerta Rosa, uma plataforma dedicada à conscientização e prevenção da violência contra a mulher. A plataforma oferece recursos educativos, fornece informações sobre o violentômetro para ajudar as mulheres a avaliarem a segurança em seus relacionamentos, e disponibiliza orientações para lidar com situações de violência." 
+                    descEn="Alerta Rosa, a platform dedicated to raising awareness and preventing violence against women. The platform offers educational resources, provides information about the violence meter to help women assess the safety of their relationships, and provides guidance for dealing with violent situations." 
+                    img="https://i.imgur.com/ZV8EQhX.png"
+                    idioma={props.idioma!}
+                    inverted={true}
+                    tech={
+                        <>
+                            <Image src={next} alt='tech' width={30} height={30} />
+                            <Image src={ts} alt='tech' width={30} height={30} />
+                            <Image src={twcss} alt='tech' width={30} height={30} />
+                            <Image src={firebase} alt='tech' width={30} height={30} />
+                        </>
+                    }
+                />
+
+                <Projeto 
                     link="https://animeflix-brasil.vercel.app" 
                     title="Animeflix"
                     descBr="O projeto Animeflix tem apenas objetivo de desafiar as capacidades no desenvolvimento de uma interface semelhante à Netflix." 
                     descEn="The Animeflix project simply aims to challenge capabilities in developing an interface similar to Netflix." 
                     img="https://i.imgur.com/Mosu60D.png"
                     idioma={props.idioma!}
-                    inverted={true}
+                    inverted={false}
+                    tech={
+                        <>
+                            <Image src={next} alt='tech' width={30} height={30} />
+                            <Image src={ts} alt='tech' width={30} height={30} />
+                            <Image src={twcss} alt='tech' width={30} height={30} />
+                        </>
+                    }
                 />
 
-                <ProjetoPrincipal 
+                <Projeto 
                     link="https://manga-shop.vercel.app/" 
                     title="MangaShop"
                     descBr="O projeto MangaShop tem o objetivo de criar uma interface para um e-commerce de vendas de mangás." 
                     descEn="The MangaShop project aims to create an interface for an e-commerce for manga sales." 
                     img="https://i.imgur.com/ENetySn.png"
                     idioma={props.idioma!}
-                    inverted={false}
+                    inverted={true}
+                    tech={
+                        <>
+                            <Image src={react} alt='tech' width={30} height={30} />
+                            <Image src={vite} alt='tech' width={30} height={30} />
+                            <Image src={ts} alt='tech' width={30} height={30} />
+                            <Image src={twcss} alt='tech' width={30} height={30} />
+                        </>
+                    }
                 />
+
+            </div>
+            <div className="flex w-full justify-center lg:justify-end">
+                <Link target="_blank" href="https://github.com/ViniciusRCSilva?tab=repositories" className="flex items-center gap-2 bg-white p-5 rounded-lg text-black">
+                    <GithubLogo/>
+                    {props.idioma == 'brasil' ? (
+                            <p>Ver mais projetos</p>
+                        ) : (
+                            <p>View more projects</p>
+                    )}
+                    <ArrowRight/>
+                </Link>
             </div>
         </div>
     )
