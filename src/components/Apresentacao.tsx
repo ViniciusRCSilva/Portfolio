e22eda31f8e32ade95c78254f07ada740ff22e18
Vinicius <viniciusflexa@gmail.com>
2024-11-05 10:20:36 -0300

    feat: update about me

--- src/components/Apresentacao.tsx
+++ src/components/Apresentacao.tsx
@@ -6,12 +6,12 @@ import Typewriter from 'typewriter-effect';
 import Image from "next/image";
 import { Parallax } from "./ParallaxBox";
 
-interface ApresentacaoProps{
+interface ApresentacaoProps {
     idioma?: string
 }
 
-export function Apresentacao(props: ApresentacaoProps){
-    return(
+export function Apresentacao(props: ApresentacaoProps) {
+    return (
         <div className="flex w-full justify-center">
             <div className="flex flex-col lg:flex-row w-[85%] justify-center lg:justify-between items-center gap-4 lg:gap-0">
                 <div className="flex lg:hidden border-4 border-[#FF6600] rounded-full p-4">
@@ -19,31 +19,37 @@ export function Apresentacao(props: ApresentacaoProps){
                 </div>
 
                 <div className="flex flex-col gap-6">
-                    <div>                        
-                        <p className="text-[#FF6600] text-6xl text-center lg:text-left">VINICIUS RODRIGUES</p>
+                    <div className="flex flex-col gap-2">
+                        <p className="text-[#FF6600] text-4xl text-center lg:text-6xl lg:text-left">VINICIUS RODRIGUES</p>
 
-                        <div className={`${props.idioma == "brasil" ? "block" : "hidden"} font-thin text-[45px] text-center lg:text-left transition-opacity`}>
+                        <div className={`${props.idioma == "brasil" ? "block" : "hidden"} font-thin text-3xl text-center lg:text-[45px] lg:text-left transition-opacity`}>
                             <Typewriter
                                 onInit={(typewriter) => {
-                                typewriter.typeString('Desenvolvedor Web')
-                                    .start();
+                                    typewriter.typeString('Desenvolvedor em evolução contínua')
+                                        .start();
                                 }}
                             />
                         </div>
-                        
-                        <div className={`${props.idioma == "eua" ? "block" : "hidden"} font-thin text-[45px] text-center lg:text-left`}>
+
+                        <div className={`${props.idioma == "eua" ? "block" : "hidden"} font-thin text-3xl text-center lg:text-[45px] lg:text-left`}>
                             <Typewriter
                                 onInit={(typewriter) => {
-                                typewriter.typeString('Web Developer')
-                                    .start();
+                                    typewriter.typeString('Continuously evolving developer')
+                                        .start();
                                 }}
-                            /> 
+                            />
                         </div>
                     </div>
 
-                    <div className="flex w-full lg:w-[20%] items-center justify-evenly">
-                        <IconesApresentacao name="CV" image={File} link="https://drive.google.com/file/d/1AkYPFrezvFBnULolRCR32mcIKreUEo6b/view?usp=sharing" />
-                        <IconesApresentacao name="GH" image={Github} link="https://github.com/ViniciusRCSilva" />
+                    <div className="flex w-full justify-center lg:justify-start">
+                        <div className="flex w-32 items-center justify-between">
+                            {props.idioma == 'brasil' ? (
+                                <IconesApresentacao name="CV" image={File} link="https://drive.google.com/file/d/1FjXoluQSPgm3XU9ZD9d0Ra-tfu451fIo/view?usp=sharing" />
+                            ) : (
+                                <IconesApresentacao name="CV" image={File} link="https://drive.google.com/file/d/1_6JoQeFPa9Jqk5kYAilM_1JKSjHbic_d/view?usp=sharing" />
+                            )}
+                            <IconesApresentacao name="GH" image={Github} link="https://github.com/ViniciusRCSilva" />
+                        </div>
                     </div>
                 </div>
 
--- src/components/SobreMim.tsx
+++ src/components/SobreMim.tsx
@@ -8,27 +8,6 @@ interface SobreMimProps {
 }
 
 export function SobreMim(props: SobreMimProps) {
-    const date = new Date
-    const [idade, setIdade] = useState<number>()
-
-    function handleIdade() {
-        let dia = date.getDate();
-        let mes = date.getMonth() + 1;
-        let ano = date.getFullYear();
-        let idade = ano - 2002 - 1;
-
-
-        if (mes > 3 || (mes === 3 && dia >= 23)) {
-            idade += 1;
-        }
-
-        setIdade(idade);
-    }
-
-    useEffect(() => {
-        handleIdade()
-    }, [])
-
     return (
         <div className="flex flex-col w-[80%] items-center justify-center gap-5">
             {props.idioma == 'brasil' ? (
@@ -37,28 +16,15 @@ export function SobreMim(props: SobreMimProps) {
 
                     <div className="flex flex-col text-4xl font-thin text-center gap-5">
                         <div className="flex items-center justify-center gap-3">
-                            <p className="text-3xl lg:text-5xl font-light">Olá!</p>
-                            <div className="flex w-12 h-12 lg:w-16 lg:h-16 justify-center items-center rounded-full bg-white p-2">
+                            <div className="flex justify-center items-center rounded-full bg-white p-4 gap-2">
+                                <p className="text-3xl lg:text-5xl font-light text-[#FF6600]">Olá!</p>
                                 <Image src={handWave} alt="waving-hand" width={50} height={50} className="hidden lg:flex" />
                                 <Image src={handWave} alt="waving-hand" width={30} height={30} className="flex lg:hidden" />
                             </div>
                         </div>
 
                         <p className="text-lg font-normal lg:text-2xl">
-                            Eu tenho {idade} anos e sou formado em Ciência da Computação.
-                        </p>
-
-                        <p className="text-lg font-normal lg:text-2xl">
-                            Destaco minha proficiência em desenvolvimento web,
-                            com habilidades avançadas em HTML, CSS, JavaScript, TypeScript e frameworks como ReactJS e NextJS.
-                            Ao longo da graduação, participei ativamente de projetos e estágio como professor de desenvolvimento web,
-                            combinando sólida base teórica com experiência prática.
-                        </p>
-
-                        <p className="text-lg font-normal lg:text-2xl">
-                            Além do desenvolvimento web, possuo conhecimentos
-                            em algoritmos, estruturas de dados e banco de dados. Destaco competências interpessoais, sendo colaborativo,
-                            comunicativo e aberto a feedback construtivo.
+                            Sou formado em Ciência da Computação, com uma base sólida em algoritmos, lógica de programação e estrutura de dados. Tenho experiência na criação de soluções práticas e eficientes por meio da construção de aplicações e na análise estratégica de dados. Além das habilidades técnicas, me destaco pela capacidade de colaborar de forma proativa, mantendo uma comunicação clara e receptiva a feedbacks construtivos, sempre focado no aprimoramento contínuo.
                         </p>
                     </div>
                 </>
@@ -68,27 +34,15 @@ export function SobreMim(props: SobreMimProps) {
 
                     <div className="flex flex-col text-4xl font-thin text-center gap-5">
                         <div className="flex items-center justify-center gap-3">
-                            <p className="text-3xl lg:text-5xl font-light">Hello!</p>
-                            <div className="flex w-12 h-12 lg:w-16 lg:h-16 justify-center items-center rounded-full bg-white p-2">
+                            <div className="flex justify-center items-center rounded-full bg-white p-4 gap-2">
+                                <p className="text-3xl lg:text-5xl font-light text-[#FF6600]">Hello!</p>
                                 <Image src={handWave} alt="waving-hand" width={50} height={50} className="hidden lg:flex" />
                                 <Image src={handWave} alt="waving-hand" width={30} height={30} className="flex lg:hidden" />
                             </div>
                         </div>
 
                         <p className="text-lg font-normal lg:text-2xl">
-                            Im {idade} years old and have a degree in Computer Science.
-                        </p>
-
-                        <p className="text-lg font-normal lg:text-2xl">
-                            I highlight my proficiency in web development,
-                            with advanced skills in HTML, CSS, JavaScript, TypeScript and frameworks such as ReactJS and NextJS.
-                        </p>
-
-                        <p className="text-lg font-normal lg:text-2xl">
-                            Throughout my degree, I actively participated in projects and internships as a web development teacher,
-                            combining a solid theoretical foundation with practical experience. In addition to web development, I have knowledge
-                            in algorithms, data structures and databases. I highlight interpersonal skills, being collaborative,
-                            communicative and open to constructive feedback.
+                            I have a degree in Computer Science, with a solid foundation in algorithms, programming logic and data structure. I have experience in creating practical and efficient solutions through building applications and strategic data analysis. In addition to my technical skills, I stand out for my ability to collaborate proactively, maintaining clear communication and receptive to constructive feedback, always focused on continuous improvement.
                         </p>
                     </div>
                 </>
