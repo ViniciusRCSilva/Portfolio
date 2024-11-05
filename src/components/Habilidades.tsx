6e408418cd88e958d49e86f8773a377e0987bf6b
Vinicius <viniciusflexa@gmail.com>
2024-11-05 10:31:02 -0300

    feat: add new habilities

--- src/components/Habilidades.tsx
+++ src/components/Habilidades.tsx
@@ -9,21 +9,27 @@ import css from '../../public/css3 1.svg'
 import js from '../../public/javascript 1.svg'
 import figma from '../../public/figma 1.svg'
 import git from '../../public/git 1.svg'
+import python from '../../public/python-logo.svg';
+import powerbi from '../../public/power_BI_Logo.svg';
+import sql from '../../public/sql-logo.svg';
 
-interface HabilidadesProps{
+interface HabilidadesProps {
     idioma?: string
 }
 
-export function Habilidades(props: HabilidadesProps){
-    return(
+export function Habilidades(props: HabilidadesProps) {
+    return (
         <div className="flex flex-col w-[80%] items-center justify-center gap-6">
             {props.idioma == 'brasil' ? (
                 <p className="text-4xl lg:text-6xl text-[#FF6600]">HABILIDADES</p>
             ) : (
                 <p className="text-4xl lg:text-6xl text-[#FF6600]">HABILITIES</p>
             )}
-            
-            <div className="w-full lg:w-[50%] grid grid-cols-3 gap-4">
+
+            <div className="w-full lg:w-[50vw] grid grid-cols-3 gap-4">
+                <Habilidade name="Python" image={python} />
+                <Habilidade name="PowerBI" image={powerbi} />
+                <Habilidade name="SQL" image={sql} />
                 <Habilidade name="ReactJS" image={react} />
                 <Habilidade name="NextJS" image={next} />
                 <Habilidade name="TypeScript" image={ts} />
